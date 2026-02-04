10faff7cc1d5cc8487188da8f23d9a5cf51ac798
Himanshu Tayade <himanshutayade031@gmail.com>
2026-02-04 13:54:22 +0530

    update

--- task-manager-frontend/src/api/api.js
+++ task-manager-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://todo-task-management-hwkf.onrender.com"
+  baseURL: "https://todo-task-management-hwkf.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
